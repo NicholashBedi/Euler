31b7313cd64484f38f73d9ae3e0d7613d11a63ec
Nicholash Bedi <nbedi@uwaterloo.ca>
2014-09-21 19:59:43 -0400

    initial-testing

---
+++ Prob_4_Palindrome_Product.cpp
@@ -0,0 +1,18 @@
+#include<cmath>
+#include<iostream>
+#include<cstdlib>
+#include<vector>
+using namespace std;
+
+//bool ispan(int num)
+//{
+//	
+//}
+
+int main()
+{
+	string a = "1234";
+	cout << a[1] << endl;
+	return EXIT_SUCCESS;
+}
+
