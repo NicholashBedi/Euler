513277a9acba679492ed9571ec9a0801e3fd4e34
Nicholash Bedi <nbedi@uwaterloo.ca>
2014-09-21 21:09:16 -0400

    9409result

--- Prob_4_Palindrome_Product.cpp
+++ Prob_4_Palindrome_Product.cpp
@@ -2,6 +2,8 @@
 #include<iostream>
 #include<cstdlib>
 #include<vector>
+#include<string>
+#include<sstream>
 using namespace std;
 
 bool ispan(string a)
@@ -15,10 +17,33 @@ bool ispan(string a)
 	return false;
 }
 
+string tostring(int f)
+{
+    string a;
+	stringstream out;
+	out << f;
+	a = out.str();
+    return a;
+}
+
 
 int main()
 {
-	cout << ispan("racecar") <<endl;
-
+	int max = 0;
+	for (int i = 1; i < 100; i++)
+	{
+		for (int j = 1; j < 100; j++)
+		{
+			
+			if (ispan(tostring(i*j)) == true)
+			{
+				if (max < i*j)
+				{
+					max = i*j;
+				}
+			}
+		}
+	}
+	cout << max << endl;
 	return EXIT_SUCCESS;
 }
