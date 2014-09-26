f475cd38b3adfef7b5811e7893f272445e1b89ae
Nicholash Bedi <nbedi@uwaterloo.ca>
2014-09-26 09:58:27 -0400

    "Complete"

--- 11/prob11.cpp
+++ 11/prob11.cpp
@@ -32,23 +32,79 @@ int main()
 		fingrid.close();
 	long long int prodmax = 1;
 	
-	for(int q = 0; q < 20; q++)				//From Right to left
+	
+	//From Right to left
+	for(int row = 0; row < 20; row++)				
 	{
-		for(int w = 0; w < 17; w++)
+		for(int coll = 0; coll < 17; coll++)
 		{	
 			int prod = 1;
 			for(int e = 0; e < 4; e++)
 			{
-				prod*=grid[(q*20)+w+e];
+				prod*=grid[(row*20)+coll+e];
 			}
 			if(prod > prodmax)
 				prodmax = prod;
 		}
 	}
-	cout << prodmax << endl;
+//	cout <<"max left to right " << prodmax << endl;
 	
 	
+	//Up down
+	for(int row = 0; row < 17; row ++)
+	{
+		for( int coll = 0; coll < 20; coll++)
+		{
+			int prod = 1;
+			for(int e = 0; e < 4; e++)
+			{
+				prod*=grid[ (row + e) * 20 + coll];
+			}
+			if(prod > prodmax)
+				prodmax = prod;
+		}
+	}
+//	cout <<"max up down " << prodmax << endl;
 	
+	//Diagonal right down
+	for(int row = 0; row < 17; row ++)
+	{
+		for( int coll = 20; coll > 3; coll--)
+		{
+			int prod = 1;
+			for(int e = 0; e < 4; e++)
+			{
+				prod*=grid[(row + e) * 20 + (coll - e)];
+			}
+			if(prod > prodmax)
+				prodmax = prod;
+		}
+	}
+//	cout <<"diag top left " << prodmax << endl;
+	
+		
+	//Diagonal left down
+	for(int row = 0; row < 17; row ++)
+	{
+		for( int coll = 0; coll < 17; coll++)
+		{
+			int prod = 1;
+			for(int e = 0; e < 4; e++)
+			{
+				prod*=grid[(row + e) * 20 + (coll + e)];
+			}
+			if(prod > prodmax)
+				prodmax = prod;
+		}
+	}
+	cout <<"Max value " << prodmax << endl;
 	return EXIT_SUCCESS;
+
 }
+/*
+Max value 70600674
+
+--------------------------------
+Process exited after 0.0585 seconds
+*/
 
