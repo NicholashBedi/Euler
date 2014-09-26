ff257457a18234994a6d68f4b319103658f30b8e
Nicholash Bedi <nbedi@uwaterloo.ca>
2014-09-25 23:53:17 -0400

    Completed 8

--- Prob8/Prob8.cpp
+++ Prob8/Prob8.cpp
@@ -4,6 +4,7 @@
 #include<cmath>
 #include<cstdlib>
 #include<fstream>
+#include<iomanip>
 using namespace std;
 
 
@@ -11,42 +12,60 @@ int main()
 {
 	ifstream fileIn("1000digitnumber.txt");
 	
-//	if (!fileIn)
-//	{
-//		cout << "Unable to open file" << endl;
-//		return EXIT_FAILURE;
-//	}
-//	fileIn.close()
-//
-//	for (int i = 0; i < 250; i++)
-//	{
-//		for (int j = 0; j < 4; j++)
-//		{
-//		
-//		}
-//	}
-//	
-//	for (int i = 0; i < 5; i++)
-//	{
-//		char a = '0';
-//		fileIn >> a;
-//		cout << a << endl;
-//	}
-//	cout << endl;
-//	
-//	fileIn.close();
-//	ifstream filein("1000digitnumber.txt");
-	
-	for (int i = 0; i < 5; i++)
+	if (!fileIn)
 	{
-		char a;
-		filein >> a;
-		int ia = a -'0';				//HOW DOES THIS WORK??
-		cout << ia +1  << endl;
+		cout << "Unable to open file" << endl;
+		return EXIT_FAILURE;
 	}
 	
+	long long int maxprod = 1;
+
+	for (int i = 0; i < 76; i++)
+	{
+		long long int prod = 1;
+		for (int j = 0; j < 13; j++)
+		{
+			char a;
+			fileIn >> a;
+			int ia = a -'0';
+			prod *= ia;
+		}
+		if (prod > maxprod)
+			maxprod = prod;
+	}
+	fileIn.close();
 	
+	for(int r = 1; r <= 13; r++)
+	{
+		ifstream fileIn("1000digitnumber.txt");
+		
+		for (int x = 0; x < r; x ++)
+		{
+			char a;
+			fileIn >> a;
+		}
+		for (int k = 0; k < 75; k++)
+		{
+			long long int prod = 1;
+			for (int l = 0; l < 13; l++)
+			{
+				char a;
+				fileIn >> a;
+				int ia = a -'0';
+				prod *= ia;
+			}
+			if (prod > maxprod)
+			maxprod = prod;
+		}
+	}
+	cout << maxprod << endl;
 	
+	fileIn.close();
+	return EXIT_SUCCESS;
 }
+
+/*
+23514624000
+*/
 
 
